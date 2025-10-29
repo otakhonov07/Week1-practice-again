979c7f9c788141ef1712a1b01d62bb5ba90d3d00
Otabek <otaxonovotabek@dmail.com>
2025-10-30 00:07:11 +0500

    Practice2

---
+++ Practice2.py
@@ -0,0 +1,8 @@
+print("Let's write a story! Please provide some words.")
+
+adj = input("Enter an adjective:")
+noun = input("Enter a noun:")
+verb_past_tense = input("Enter a verb (pst tense):")
+
+print("\nHere is your story:")
+print("The" + adj + " " + noun + " " + verb_past_tense + "over the dog.")

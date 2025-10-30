3ad485761bd797ea1dc9651b1dce4b0d5945da5c
Otabek <otaxonovotabek@dmail.com>
2025-10-30 14:20:30 +0500

    Add Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,18 @@
+name = input("What's your full name?")
+planet = input("What's your home planet?")
+species = input("Which species do you have?")
+rank = input("What's your official rank?")
+idnumber = input("What's your ID Number?")
+
+print("========================================")
+print("      GALACTIC FEDERATION ID CARD")
+print("========================================")
+
+print("Full name:", name)
+print("Home Planet:", planet)
+print("Species:", species)
+print("Rank:", rank)
+
+print("----------------------------------------")
+print("ID Code: GF - " + idnumber + "-" + planet )
+print("========================================")

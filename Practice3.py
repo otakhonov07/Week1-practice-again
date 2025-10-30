bc1922776d57eb5cdece7a20757120583e7b1958
Otabek <otaxonovotabek@dmail.com>
2025-10-30 09:38:11 +0500

    Add Practice3.py, a simple mad libs style program

---
+++ Practice3.py
@@ -0,0 +1,11 @@
+
+
+name = input("What's your name?")
+major = input("What's your major?")
+hobby = input("What's your favourite hobby?")
+
+print("About you:")
+
+print("\nYour name is", name)
+print("Your major is", major)
+print("Your favourite hobby is", hobby)
